2e2da9870bcce29e8e18df8b28ad89ced3cd244b
dolsotbob <1hyoon3@naver.com>
2025-04-08 11:35:21 +0900

    modified

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Erc20
+
+
+# Checking deployed contract on KaiaScope
+https://kairos.kaiascope.com/account/0xA01167E96DE2d39F49580d3a40eBEFa538B2a2F0?tabId=approvals&sub=kip7
--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,1 +1,11 @@
-contract MyToken {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("My Token", "MTK") {
+        // (나) 1000개의 토큰을 ERC20 표준에 맞게 소수 18자리로 표현해서 민팅해주는 코드
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+}
