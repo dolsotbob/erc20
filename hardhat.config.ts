460747e6fbbfeda8ab92506b9e5f1574494046f8
dolsotbob <1hyoon3@naver.com>
2025-04-08 14:20:35 +0900

    modified

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,8 +7,8 @@ const config: HardhatUserConfig = {
   solidity: '0.8.28',
   networks: {
     ganache: {
-      // url: 'HTTP://127.0.0.1:7545', // Todo: Ganache RPC URL
-      url: 'https://public-en-kairos.node.kaia.io', // kaia rpc 로 변경 
+      url: 'HTTP://127.0.0.1:7545', // Todo: Ganache RPC URL
+      // url: 'https://public-en-kairos.node.kaia.io', // kaia rpc 로 변경 
       accounts: [
         process.env.PRIVATE_KEY || '', // Todo: Ganache에서 제공하는 프라이빗 키 사용(.env 파일을 사용합니다)
       ],
